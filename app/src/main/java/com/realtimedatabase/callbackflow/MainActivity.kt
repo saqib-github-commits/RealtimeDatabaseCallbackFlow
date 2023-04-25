2fcfe5b18ada40a20c30fb40e9bd384900fa78aa
saqib <saqib@saqib.com>
2023-04-25 10:33:23 +0200

    firebase realtime database callbackFlow example

--- app/src/main/java/com/realtimedatabase/callbackflow/MainActivity.kt
+++ app/src/main/java/com/realtimedatabase/callbackflow/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.realtimedatabase.callbackflow.firebase.FirebaseDatabaseListeners
 import com.realtimedatabase.callbackflow.ui.theme.RealtimeDatabaseCallbackFlowTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             RealtimeDatabaseCallbackFlowTheme {
                 // A surface container using the 'background' color from the theme
@@ -26,6 +28,12 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        FirebaseDatabaseListeners.readFirebaseRealtimeDatabase()
     }
 }
 
